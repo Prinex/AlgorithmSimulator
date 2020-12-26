f7ce18f6bff27c948050909e1da932f26760dc97
Prinex <songdahye00@gmail.com>
2020-12-26 18:15:06 +0000

    Implemented Bubble Sort with a GUI simulation

--- SortingAlgorithmsSrc/BubbleSort.cpp
+++ SortingAlgorithmsSrc/BubbleSort.cpp
@@ -1,1 +1,55 @@
+#include "include/SortingAlgorithms.h"
+#include "include/Interface.h"
 
+
+BubbleSort::BubbleSort(int min, int max, int els, sf::RenderWindow& win) :
+	SortingAlgorithms(min, max, els, win)
+{}
+
+int BubbleSort::Sort(std::unique_ptr<Interface>& init)
+{
+	int sortedElement = 0;
+	while (window.isOpen())
+	{
+		for (unsigned int i = 0; i < sequence.size(); i++)
+		{
+			for (std::vector<sf::RectangleShape>::iterator itj = sequence.begin(); itj != sequence.end() - 1; itj = std::next(itj))
+			{
+				if ((*itj).getSize().y > (*std::next(itj)).getSize().y)
+				{
+					// hoovering effect
+					(*itj).setFillColor(sf::Color::Red);
+					(*std::next(itj)).setFillColor(sf::Color::Red);
+
+					// Swapping inside the vector
+					std::swap(*itj, *std::next(itj));
+
+					// Swapping the coordinates (x, y)
+					float x = (*itj).getPosition().x;
+					float y = (*itj).getPosition().y;
+
+					float x_next = (*std::next(itj)).getPosition().x;
+					float y_next = (*std::next(itj)).getPosition().y;
+
+					(*itj).setPosition(sf::Vector2f(x_next, y_next));
+					(*std::next(itj)).setPosition(sf::Vector2f(x, y));
+
+					// Printing
+					window.clear(sf::Color::White);
+					for (std::vector<sf::RectangleShape>::iterator itp = sequence.begin(); itp != sequence.end(); itp = std::next(itp))
+					{
+						std::cout << (*itp).getSize().y << '\n';
+						window.draw(*itp);
+					}
+					window.display();
+
+					// hoovering effect
+					(*itj).setFillColor(sf::Color::Black);
+					(*std::next(itj)).setFillColor(sf::Color::Black);
+				}
+			}
+		}
+		return PrintSortedSeq(init);
+	}
+	return 0;
+}

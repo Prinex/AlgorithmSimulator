55fe5c12c21fc72f4b62dda3b4e9983700503bc5
Prinex <songdahye00@gmail.com>
2020-12-26 22:26:43 +0000

    Standard definition of Heap Sort

---
+++ SortingAlgorithmsSrc/HeapSort.cpp
@@ -0,0 +1,12 @@
+#include "include/SortingAlgorithms.h"
+#include "include/Interface.h"
+
+
+HeapSort::HeapSort(int min, int max, int els, sf::RenderWindow& win) :
+	SortingAlgorithms(min, max, els, win)
+{}
+
+int HeapSort::Sort(std::unique_ptr<Interface>& init)
+{
+	return 0;
+}

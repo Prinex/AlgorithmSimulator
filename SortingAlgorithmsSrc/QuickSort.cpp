def1be0d78666de19442d6799ceb6ff8e6822ecf
Prinex <songdahye00@gmail.com>
2020-12-26 22:24:53 +0000

    Standard definition of Quick Sort

---
+++ SortingAlgorithmsSrc/QuickSort.cpp
@@ -0,0 +1,11 @@
+#include "include/SortingAlgorithms.h"
+#include "include/Interface.h"
+
+QuickSort::QuickSort(int min, int max, int els, sf::RenderWindow& win) :
+	SortingAlgorithms(min, max, els, win)
+{}
+
+int QuickSort::Sort(std::unique_ptr<Interface>& init)
+{
+	return 0;
+}

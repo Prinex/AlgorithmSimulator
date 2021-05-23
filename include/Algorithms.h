c69b4e7684850d0b14558c0a77c9e4f0bd45a293
calotad <calotad@github.coventry.ac.uk>
2021-05-23 20:41:22 +0100

    Expanded the explanation of the Sort method

--- include/Algorithms.h
+++ include/Algorithms.h
@@ -40,8 +40,11 @@ public:
 	SortingAlgorithms(int min, int max, int els, sf::RenderWindow& win);
 
 	/** Sort is a pure virtual method for overriding the Sort method for different implementations 
-	 *  of the sorting algorithms and param init 
-	 *  which is what I described about win param from SortingAlgorithms constructor 
+	 *  of the sorting algorithms and param init which is what I described about win param from 
+	 *  SortingAlgorithms constructor 
+	 *	This pure virtual method can't be overriden with different number of parameters, but it can
+	 *  be used as an entry point to an actual method with different no. of paramteres, e.g. merge
+	 *  sort or quick sort
 	 *  @param init - a referenced object to the first initialization of the window object
 	 *  @return back where it was called last time (menu feature / navigating)
 	 */

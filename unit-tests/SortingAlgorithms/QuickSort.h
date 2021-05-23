44b7b7d337370632f3900300852ac2e4afc43e50
calotad <calotad@github.coventry.ac.uk>
2021-05-23 21:58:36 +0100

    Commented QuickSort's code

--- unit-tests/SortingAlgorithms/QuickSort.h
+++ unit-tests/SortingAlgorithms/QuickSort.h
@@ -7,32 +7,39 @@
 
 std::vector<int> QuickSort(std::vector<int>& sequence, int left, int right)
 {
-	int pivot = (left + right) / 2;
-	int l = left;
-	int r = right;
+	int p = (left + right) / 2;
+	int pivot = sequence.at(p);										// median pivot
+	int l = left;													// left marker
+	int r = right;													// right marker
 
-	while (l <= r)
-	{
-		while (sequence.at(l) < sequence.at(pivot))
-		{
-			l++;
+	while (l <= r)													// while the left marker is not beyond the
+	{																// right marker or if they are at the same position
+		while (sequence.at(l) < pivot)								// while the left marker is less
+		{															// than the pivot
+			l++;													// move the left marker to the right
 		}
-		while (sequence.at(r) > sequence.at(pivot))
-		{
-			r--;
+		while (sequence.at(r) > pivot)								// while the right marker is greater
+		{															// than the pivot
+			r--;													// move the right marker to the left
 		}
-		if (l <= r)
-		{
-			std::swap(sequence.at(l), sequence.at(r));
-			if (pivot == l) pivot = r;
-			else if (pivot == r) pivot = l;
-			l++;
-			r--;
+		if (l <= r)													// if the left marker is not beyond the
+		{															// right marker or if they are at the same position
+			std::swap(sequence.at(l), sequence.at(r));				// swap the markers
+			if (p == l)												// if the left marker ran into the pivot
+			{
+				p = r;
+			}
+			else if (p == r)				 						// if the right marker ran into the pivot
+			{
+				p = l;
+			}
+			l++;													// move the left marker to the right
+			r--;													// move the right marker to the left
 		}
 	}
-	if (left < l)
-		QuickSort(sequence, left, r);
-	if (r < right)
-		QuickSort(sequence, l, right);
-	return sequence;
+	if (left < r)													// recurse the left side of elements 
+		QuickSort(sequence, left, r);								// less than the pivot
+	if (l < right)													// recurse the right side of elements
+		QuickSort(sequence, l, right);								// bigger than the pivot
+	return sequence;												// return the sorted sequence
 }

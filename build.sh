a1d94977b39d667e03ebd8a13aa742198b138cda
calotad <calotad@github.coventry.ac.uk>
2021-06-20 23:41:36 +0100

    Completely automated the script file

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo "Choose a build option: "
-echo "1. Build the project using VS 16 2019 x64"
-echo "2. Build the project using MinGW Makefiles on either Linux or Windows."
-echo "2. Build the project using Unix Makefiles."
-
-read OPTION
-
 echo "Building the project..."
 rm -rf build
 rm -rf bin
@@ -14,14 +7,11 @@ rm -rf lib
 
 mkdir build
 cd build
-
-if [ $OPTION == 1 ]
+# build the project accordingly to the OS' specified above
+if [ $OSTYPE == "msys" ]
 then
 	cmake .. -G "Visual Studio 16 2019"
-elif [ $OPTION == 2 ]
-then
-	cmake .. -G "MinGW Makefiles"
-elif [ $OPTION == 3 ]
+elif [ $OSTYPE == "linux" ]
 then
 	cmake .. -G "Unix Makefiles"
 else
